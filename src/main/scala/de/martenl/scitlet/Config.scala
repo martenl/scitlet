82355dc19f1cc9a0c9b7c20d5e33445c59c80131
martenl <martenjanlosansky@gmail.com>
2016-10-23 16:24:03 +0200

    Added methods to read and parse config files.

--- src/main/scala/de/martenl/scitlet/Config.scala
+++ src/main/scala/de/martenl/scitlet/Config.scala
@@ -2,15 +2,33 @@ package de.martenl.scitlet
 
 import java.io.File
 
+import de.martenl.scitlet.Utils._
+
 object Config {
 
   def createAt(path:String):Unit = {
     Files.createFile(path+File.separator+"config")
   }
+
   def isBare():Boolean ={
+    //read("core")("bare")
     true
   }
 
+  def read():ConfigMap = {
+    read(Files.scitletPath()+File.separator+"config")
+  }
+
+  def read(configFilePath:String):ConfigMap={
+    val configFileContent = Files.read(configFilePath)
+    for(line <- configFileContent.split("\n")){
+      line.charAt(0) match {
+        case '['  => println(s"config entry name: ${line.replace("[","").replace("]","")}")
+        case '\t' => println(s"config entry property name: ${line.split("=")(0)} value: ${line.split("=")(1)}")
+      }
+    }
+    ConfigMap(List())
+  }
 
   def objToStr(configData:ConfigData):String = {
     configData match{
@@ -20,6 +38,7 @@ object Config {
   }
 
   def strToObj(data:String):ConfigData = {
+    val indices = getAllIndices(data,"[")
     ConfigMap(List())
   }
 
@@ -27,5 +46,7 @@ object Config {
 
   case class ConfigEntry(key:String,value:String) extends ConfigData
 
-  case class ConfigMap(entries:List[ConfigData]) extends ConfigData
+  case class ConfigMap(entries:List[ConfigData]) extends ConfigData {
+
+  }
 }
