1ba9f8bb7858d50ea45505f654019788e67e3999
martenl <martenjanlosansky@gmail.com>
2016-10-23 14:15:39 +0200

    Zip content instead of creating zip file with file in it.

--- src/main/scala/de/martenl/scitlet/Zipper.scala
+++ src/main/scala/de/martenl/scitlet/Zipper.scala
@@ -1,27 +1,49 @@
 package de.martenl.scitlet
 
-import java.io.{BufferedOutputStream, File, FileInputStream, FileOutputStream}
-import java.util.zip.{ZipEntry, ZipOutputStream}
+import java.io.{File, FileInputStream, FileOutputStream}
+import java.util.zip._
 
-/**
-  * Created on 03.07.2016.
-  */
 object Zipper {
 
   def zipFile(path:String,destination:String,fileName:String):Unit = {
-    val file = new File(path)
-    val in = new FileInputStream(file)
-    val bytes = new Array[Byte](file.length().toInt)
-    in.read(bytes)
-    in.close()
+    val content = Files.read(path)
+    val bytes = content.getBytes
+    val deflater = new Deflater()
+    deflater.setInput(bytes)
+    val bytesToWrite = new Array[Byte](100)
     Files.createDirectory(destination)
     val destFile = destination+File.separator+fileName
-    if(Files.existsFile(destination+File.separator+fileName)){
-      return
+    val out = new FileOutputStream(destFile)
+    while(!deflater.finished()){
+      deflater.deflate(bytesToWrite)
+      out.write(bytesToWrite)
     }
-    val stream = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(destFile)))
-    stream.putNextEntry(new ZipEntry(fileName))
-    stream.write(bytes)
-    stream.close()
+    out.close()
+  }
+
+  def unzipFile(path:String):String = {
+    Files.existsFile(path) match {
+      case true => {
+        val bytes = getBytes(path)
+        val inflater = new Inflater()
+        inflater.setInput(bytes)
+        val builder = new StringBuilder()
+        val output = new Array[Byte](1024)
+        while (!inflater.finished()){
+          inflater.inflate(output)
+          builder.append(new String(output))
+        }
+        println(builder.toString().trim.size)
+        builder.toString().trim
+      }
+      case false => "Nothing found"
+    }
+  }
+
+  def getBytes(path:String):Array[Byte] = {
+    val reader:FileInputStream = new FileInputStream(path)
+    val bytes = new Array[Byte]( reader.available())
+    reader.read(bytes)
+    bytes
   }
 }
