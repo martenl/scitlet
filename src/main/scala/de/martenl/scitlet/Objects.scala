79e6d96a04b9240115d7cd43518019a94a5be6cb
martenl <martenjanlosansky@gmail.com>
2016-10-23 15:34:08 +0200

    Create functionality to store Blobs.

--- src/main/scala/de/martenl/scitlet/Objects.scala
+++ src/main/scala/de/martenl/scitlet/Objects.scala
@@ -8,9 +8,6 @@ object Objects {
     Files.createDirectory(Files.combine(objectsPath,"info"))
     Files.createDirectory(Files.combine(objectsPath,"pack"))
   }
-}
-
-class Objects {
 
   def addFile(path:String):String = {
     val objectFile = Hasher.hashFile(path)
@@ -18,10 +15,13 @@ class Objects {
     val fileName = objectFile.substring(2)
     val destination:String = Files.scitletPath() + Files.combine("","objects",directoryName)
     Zipper.zipFile(path,destination,fileName)
-   objectFile
+    objectFile
   }
 
   def store(scitletObject:Object):Unit={
-
+    scitletObject match {
+      case Tree(path,hash,mode,objects) => ;
+      case Blob(path,hash,mode) => addFile(path)
+    }
   }
 }
