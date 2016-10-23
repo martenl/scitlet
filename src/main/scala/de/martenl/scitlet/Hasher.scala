5822c029861330ba766682e15c0deb8c96c98811
martenl <martenjanlosansky@gmail.com>
2016-10-23 14:20:04 +0200

    Use helper method form Files to read file and add method to hash tree object.

--- src/main/scala/de/martenl/scitlet/Hasher.scala
+++ src/main/scala/de/martenl/scitlet/Hasher.scala
@@ -1,6 +1,5 @@
 package de.martenl.scitlet
 
-import java.io.{File, FileInputStream}
 import java.math.BigInteger
 import java.security.MessageDigest
 
@@ -12,16 +11,17 @@ object Hasher {
   val hasher:MessageDigest = MessageDigest.getInstance("sha-1")
 
   def hashFile(path:String):String = {
-    val file = new File(path)
-    val in = new FileInputStream(file)
-    val bytes = new Array[Byte](file.length().toInt)
-    in.read(bytes)
-    in.close()
-    hasher.update(bytes)
-    String.format("%x",new BigInteger(1,hasher.digest()))
+    val content = Files.read(path)
+    val store = s"blob ${content.length}\\0$content"
+    hashString(store)
   }
 
-  def hashString(data:String):String = {
+  def hashTree(content: String) = {
+    val store = s"tree ${content.length}\\0$content"
+    hashString(store)
+  }
+
+  private def hashString(data:String):String = {
     hasher.update(data.getBytes())
     String.format("%x",new BigInteger(1,hasher.digest()))
   }
