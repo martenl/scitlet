03d9426edc4190270a55895e1e681883be1e6e33
martenl <martenjanlosansky@gmail.com>
2016-11-19 14:07:08 +0100

    Implemented functionality correctly

--- src/main/scala/de/martenl/scitlet/Head.scala
+++ src/main/scala/de/martenl/scitlet/Head.scala
@@ -1,32 +1,42 @@
 package de.martenl.scitlet
 
-import java.io.File
-
-/**
-  * Created on 22.10.2016.
-  */
 object Head {
 
   val path:String = Files.combine(Files.scitletPath(),"HEAD")
+  val headsPath = Files.combine(Files.scitletPath(),"refs","heads")
 
-  def getCurrentHead(): String ={
-    Files.read(path).replace("ref: ","")
+  def get(): Option[String] ={
+    Files.existsFile(path) match{
+      case true => getByPath(Files.combine(Files.scitletPath(),Files.read(path).replace("ref: ","")))
+      case false => None
+    }
+
   }
 
-  def setCurrentHead(value:String): Unit ={
-
+  def set(name:String,value:String): Unit ={
+    Files.write(path,s"ref: refs/heads/$name")
+    setByName(name,value)
   }
 
-  def getHead(name:String):Option[String] = {
-    val refPath = "refs" + File.separator + "heads" + File.separator + name
+  def getByPath(refPath:String):Option[String] = {
     Files.existsFile(refPath) match {
       case true => Some(Files.read(refPath).replace("refs: ",""))
       case false => None
     }
   }
 
-  def setHead(name:String,value:String):Unit = {
-    val refPath = "refs" + File.separator + "heads" + File.separator + name
-    Files.write(refPath,s"refs: $value")
+  def getByName(name:String):Option[String] = {
+    val refPath = Files.combine(headsPath,name)
+    getByPath(refPath)
+  }
+
+  def setByPath(refPath:String, value:String):Unit = {
+    Files.createDirectory(headsPath)
+    Files.write(refPath,s"$value\n")
+  }
+
+  def setByName(name:String, value:String):Unit = {
+    val refPath = Files.combine(headsPath,name)
+    setByPath(refPath,value)
   }
 }
